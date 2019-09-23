3bdde46bb0b62c14e9aebfe449e78d71bc0ce825
Roman Loboda <whiten.ncs@gmail.com>
2019-09-23 18:14:40 +0300

    Post api/lists (#9)
    * add Font Awesome
    
    * finish route GET /lists
    
    * remove bakespace
    
    * add link to create check box card
    
    * post /api/lis

--- routes/ListRoute.js
+++ routes/ListRoute.js
@@ -1,11 +1,12 @@
 const express = require('express');
 var ObjectID = require('mongodb').ObjectID;
 const router = express.Router();
+const config = require("../config/db");
 
 
 module.exports.routeList = function (db) {
     router.get('/', function (req, res) {
-        res.render('addList')
+        res.render('addList');
     });
 
     return router;
@@ -13,8 +14,55 @@ module.exports.routeList = function (db) {
 
 
 module.exports.routeAPIList = function (db) {
-    router.get('/:id?', function () {
+    router.post('/', function (req, res) {
+        const id_client = req.body.id_client;
+        const lists = req.body.data.check_box;
 
+        if (id_client && Array.isArray(lists) && lists.length > 0) {
+            // проверка клиента в базе
+            const filterDb = { _id: ObjectID(id_client) };
+
+            db.collection(config.collection.client).findOne(filterDb, function (err, result) {
+                if (err) {
+                    res.send('error select mongo');
+                } else {
+
+                    // Клиент найден
+                    if (result) {
+                        const insertOb = {
+                            id_client: req.body.id_client,
+                            type: 'CheckBoxCard',
+                            data: {
+                                    title: req.body.data.title || '',
+                                    is_img: req.body.data.is_img || false,
+                                    check_box: req.body.data.check_box
+                                }
+                            };
+
+                        db.collection(config.collection.card).insertOne(insertOb, (err, result) => {
+                            if (err){
+                                res.status(404)
+                                res.send('error select mongo');
+                            } else {
+                                res.send(result.insertedId)
+                            }
+                        })
+
+                    } else {
+                        res.status(404)
+                        res.send("id_client not found");
+                    }
+                }
+            })
+        } else {
+            if (!id_client) {
+                res.status(404)
+                res.send("id_client is required");
+            } else {
+                res.status(404)
+                res.send("check_box array length must more 0");
+            }
+        }
     });
 
     return router;
