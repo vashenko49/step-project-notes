c5f7e30a80580b2dec9ed09d8276678a163ceac0
Roman Loboda <whiten.ncs@gmail.com>
2019-09-23 13:20:19 +0000

    remove bakespace

--- front/js/index.js
+++ front/js/index.js
@@ -1,12 +1,9 @@
 import {Authorizatoin} from "./modules/Authorizatoin";
 import {Services} from "./modules/Services";
 import {Card} from "./modules/Card";
-<<<<<<< HEAD
-import { CreateList } from "./modules/CreateList"
-=======
+import {CreateList} from "./modules/CreateList"
 import {UsuallCard} from "./modules/UsuallCard";
 import {CheckBoxCard} from "./modules/CheckBoxCard";
->>>>>>> a3d21e050d6ab53592e1124375583a4421ad2e6b
 
 
 $().ready(function () {
@@ -24,12 +21,9 @@ $().ready(function () {
                 })
         });
 
-
-<<<<<<< HEAD
     $(document).on('click',"#addNewItemList", CreateList.addItemList);
     $(document).on('click',"#removeItemList", CreateList.removeItemList);
 
-=======
     $(document).on('click',".UsuallCard .remove-card", UsuallCard.RemoveUsualCard);
     $(document).on('click',".UsuallCard .change-card", UsuallCard.ChangeUsualCard);
     $(document).on('click',".UsuallCard .more-information", UsuallCard.MoreUsualCard);
@@ -37,5 +31,4 @@ $().ready(function () {
     $(document).on('click',".CheckBoxCard .remove-card", CheckBoxCard.RemoveCheckBoxCard);
     $(document).on('click',".CheckBoxCard .change-card", CheckBoxCard.ChangeCheckBoxCard);
     $(document).on('click',".CheckBoxCard .more-information", CheckBoxCard.MoreCheckBoxCard);
->>>>>>> a3d21e050d6ab53592e1124375583a4421ad2e6b
 });
