a3d21e050d6ab53592e1124375583a4421ad2e6b
Sergey <vashenko49@gmail.com>
2019-09-23 15:49:10 +0300

    fixed

--- front/js/modules/Card.js
+++ front/js/modules/Card.js
@@ -3,46 +3,6 @@ export class Card {
         $(`#${id}`).parent().parent().remove();
     }
 
-}
-        let $target = $(event.target);
-        let type = $target.attr('data-typeBtn').toUpperCase();
-        if(type){
-            let idCard = $target.parent().attr('id');
-            console.log(idCard);
-            if(type==="remove-card".toUpperCase()){
-                if($target.parent().attr('data-type').toUpperCase()==="UsuallCard".toUpperCase()){
-                    console.log('rem usua');
-                    
-
-                }else {
-                    console.log('rem check');
-
-
-                }
-            }else if(type==="change-card".toUpperCase()){
-                if($target.parent().attr('data-type').toUpperCase()==="UsuallCard".toUpperCase()){
-                    console.log('change usua');
-
-
-                }else {
-                    console.log('change check');
-
-
-                }
-            }else if(type==='more-information'.toUpperCase()){
-                if($target.parent().attr('data-type').toUpperCase()==="UsuallCard".toUpperCase()){
-                    console.log('more usua');
-
-
-                }else {
-                    console.log('more check');
-
-
-                }
-            }
-        }
-    }
-
     static removeCard(id){
         $(`#${id}`).parent().parent().remove();
     }
