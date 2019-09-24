74c40a9d1020b4838ecab6ed6d587f58c8668354
Ramazan Khanbabaev <ramazan.khanbabaev@gmail.com>
2019-09-24 23:54:32 +0300

    post api/notes unfinished

--- front/js/modules/UsuallCard.js
+++ front/js/modules/UsuallCard.js
@@ -1,6 +1,41 @@
 import {Card} from "./Card";
+import {Authorizatoin} from "./Authorizatoin";
 
 export class UsuallCard extends Card{
+    static createUsualNote(event) {
+        event.preventDefault();
+
+        const sendData = {
+            id_client: Authorizatoin.GetIdClient(),
+            data: {
+                title: '',
+                text: ''
+            }
+        };
+
+        const form = $(this).parent('form');
+        $(form).find('input').each(function() {
+            const input = $(this);
+            if (input.id === 'title') {
+                sendData.data.title = input.value;
+            } else if (input.id === 'text') {
+                sendData.data.text = input.value
+            }
+        });
+
+        $.ajax({
+            type: 'POST',
+            url: '/api/notes',
+            data: sendData
+        })
+            .done(function(res) {
+            window.location = '/';
+        })
+            .fail(function(err) {
+            throw new Error(err);
+        })
+
+    }
 
     static RemoveUsualCard(event){
         console.log('usual rem');
