fc27a4e16c17e0b2172e67875029e19f0ca01773
Vashchenko Sergey Aleksandrovich <33183663+vashenko49@users.noreply.github.com>
2019-09-25 17:08:01 +0300

    fixed logout (#27)
    * fixed checkbox
    
    * created method getidclient
    
    * fixed burron
    
    * fixed button
    
    * fixed gulp file task 'libs'
    
    * fixed header link, fixed gulp file
    
    * fixed header link, fixed gulp file
    
    * fixed cookie
    
    * fixed cookie
    
    * fixed cookie
    
    * start heroku server
    
    * fixed package.json
    
    * make it better
    
    * merge with develop for merge with master
    
    * fixed logout

--- front/js/modules/Authorizatoin.js
+++ front/js/modules/Authorizatoin.js
@@ -100,6 +100,6 @@ export class Authorizatoin {
 
     static LogOut(){
         document.cookie="authorization=0; expires=Thu, 01 Jan 1970 00:00:01 GMT; path=/";
-        window.location.reload();
+        window.location.href = '/';
     }
 }
